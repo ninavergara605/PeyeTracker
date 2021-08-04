e3a4c3742245d245f58b01a0a5d24f215301ae73
ninavergara605 <60552438+ninavergara605@users.noreply.github.com>
2021-08-04 02:50:16 -0400

    Delete PeyeTracker directory

--- PeyeTracker/Roi/__init__.py
+++
@@ -1,4 +0,0 @@
-from .get_fix_roi import GetFixationRoi
-from .get_test_roi import get_test_roi
-from .cartesian_to_raster import raster_conversion
-__all__ = ["get_test_roi", "GetFixationRoi","raster_conversion"]
