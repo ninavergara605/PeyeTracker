519f54f98462bc3a9be5b40a604ff3575ef45851
ninavergara605 <ninavergara2@gmail.com>
2022-02-07 16:01:54 -0500

    pep8 refactor

--- scripts/GetEyeMovements/get_fixations.py
+++ scripts/GetEyeMovements/get_fixations.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class GetFixations:
-# Convert this class to function
+    # Convert this class to function
 
     def __init__(self, raw_fixations, metadata_keys):
         self._raw_fixations = raw_fixations
@@ -12,17 +12,14 @@ class GetFixations:
 
     def get_fixations(self):
         fixations = self._raw_fixations.copy()
-        
-        fixations.rename(columns={
-                                    5:'x' 
-                                    ,6:'y' 
-                                    ,7:'avg_pupil_size'
-                                    }, inplace=True)
 
-        fixations = fixations.groupby(self._metadata_keys).filter(lambda x: len(x) > 1) #returns trials with more than one fixation
+        fixations.rename(columns={
+            5: 'x'
+            , 6: 'y'
+            , 7: 'avg_pupil_size'
+        }, inplace=True)
+
+        fixations = fixations.groupby(self._metadata_keys).filter(
+            lambda x: len(x) > 1)  # returns trials with more than one fixation
         fixations['fix_id'] = np.arange(1, len(fixations) + 1)
         return fixations
-    
-
-
-    
