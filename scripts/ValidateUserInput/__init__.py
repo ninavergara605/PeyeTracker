84b6815a5ed09a05929e0666e364e23792b7f106
ninavergara605 <ninavergara2@gmail.com>
2022-02-21 17:04:46 -0500

    updated lower strings function name

--- scripts/ValidateUserInput/__init__.py
+++ scripts/ValidateUserInput/__init__.py
@@ -1,4 +1,3 @@
-
-
+from .lower_strings import lower_strings_dispatch
 from .user_input import validate_user_input
-__all__ = ["validate_user_input"]
+__all__ = ["validate_user_input", "lower_strings_dispatch"]
