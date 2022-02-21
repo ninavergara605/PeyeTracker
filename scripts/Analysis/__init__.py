59cad0e162081e76658d725817bd3b575f416f89
ninavergara605 <ninavergara2@gmail.com>
2022-02-21 17:23:26 -0500

    updated binning name

--- scripts/Analysis/__init__.py
+++ scripts/Analysis/__init__.py
@@ -1,4 +1,4 @@
-from .Bin.bin_movements import bin_fixations
+from .Bin.bin_movements import BinningDispatch
 from .Bin.get_time_bin_ranges import GetTimeBinRanges
 from .Bin.get_stim_locked_bins import GetStimLockedBins
 from .Bin.bin_by_rt import bin_by_rt
@@ -6,7 +6,7 @@ from .Entropy.create_matrices import CreateTransitionMatrices
 from .Entropy.calculate_entropy import CalculateEntropy
 from .Summary.binning_summary import create_bin_summaries
 
-__all__ = ["bin_fixations"
+__all__ = ["BinningDispatch"
     , "GetTimeBinRanges"
     , 'GetStimLockedBins'
     , 'bin_by_rt'
