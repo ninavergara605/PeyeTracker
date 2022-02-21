3c1a04ac56b49e8f93d784668e5d837f7ccccae9
ninavergara605 <ninavergara2@gmail.com>
2022-02-21 17:24:17 -0500

    added raster conversion function

--- scripts/Roi/__init__.py
+++ scripts/Roi/__init__.py
@@ -1,4 +1,5 @@
 from .get_fix_roi import GetFixationRoi
 from .get_test_roi import get_test_roi
+from .raster_conversion import ShiftOrigin
 
-__all__ = ["get_test_roi", "GetFixationRoi"]
+__all__ = ["get_test_roi", "GetFixationRoi", "ShiftOrigin"]
