e0196ad4153947f285766dd53c71599745371d1c
ninavergara605 <ninavergara2@gmail.com>
2022-02-21 17:10:24 -0500

    added output directory func name

--- scripts/PathUtilities/__init__.py
+++ scripts/PathUtilities/__init__.py
@@ -1,4 +1,8 @@
 from .get_directory_paths import GetPathsFromDirectory, LabelPaths
-from .general_path_functions import normalize_path, create_path
+from .general_path_functions import normalize_path, create_path, create_output_directory
 
-__all__ = ['GetPathsFromDirectory', 'LabelPaths', 'normalize_path', 'create_path']
+__all__ = ['GetPathsFromDirectory'
+            , 'LabelPaths'
+            , 'normalize_path'
+            , 'create_path'
+            , 'create_output_directory']
