ddb2dacb1e4af6805674e982e596677a0d903193
ninavergara605 <ninavergara2@gmail.com>
2022-02-21 17:22:04 -0500

    pep8 refactor

--- scripts/Import/__init__.py
+++ scripts/Import/__init__.py
@@ -1,7 +1,9 @@
 from .roi_template_import import ImportRoiTemplate
 from .format_csv import format_csv
 from .import_roi_event_map import ImportEventMaps
+from .Import_iPosition_coordinates import ImportIposition
 
 __all__ = ["ImportRoiTemplate"
-        ,"format_csv"
-        ,"ImportEventMaps"]
+    , "format_csv"
+    , "ImportEventMaps"
+    , "ImportIposition"]
